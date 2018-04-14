830896b6697044413e0145529cf005eb7ca2b39d
nan <50326921@qq.com>
2018-04-14 22:33:03 +0800

    初始化

---
+++ src/hello/hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+
+    public static void main(String[] args) {
+        int i = 10;
+        Integer j = 10;
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
